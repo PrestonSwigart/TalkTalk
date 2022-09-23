595e2655660d8b6ffea0dd3d97b0ff06f96c427d
Preston Swigart <prestonlswigart@gmail.com>
2022-09-23 08:47:16 -0400

    talktalk 9/23 8:47am

---
+++ src/HolyGrail.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+/**
+ * simple i/o project using scanner
+ * @version 9.22.22
+ * @author PSwigart
+ */
+
+public class HolyGrail {
+    /**
+     * main method of class HolyGrail
+     * @param args command line argumenrss if needed
+     */
+    public static void main(String[] args) {
+        // declaring local variables
+        String name, quest, color;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("* A chat with the bridge keeper *\n" +
+                "Who would cross the Bridge of Death must answer me these questions three, ere the other side he see.");
+        // getting user input
+        System.out.print("What is your name? ");
+        name = in.nextLine();
+        System.out.print("What is your quest? ");
+        quest = in.nextLine();
+        System.out.print("What is your favorite color? ");
+        color = in.nextLine();
+
+        System.out.println("King Arthur says, \"You have to know these things when you're a king, you know.\"");
+        // summary of user input
+        System.out.println("Your name is: " + name);
+        System.out.println("Your quest is: " + quest);
+        System.out.println("Your favorite color is " + color);
+
+    }
+}
